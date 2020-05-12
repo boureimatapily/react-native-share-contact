2a5e09524bfdcae815b78cc256402f2f7fbca454
boureimatapily <boureimatapily1@gmail.com>
2020-05-12 12:05:08 +0000

    ADD Stack navigation

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import {createStackNavigator} from '@react-navigation/stack'
+import LoginScreen from './src/screen/LoginScreen';
+import RegisterScreen from './src/screen/RegisterScreen';
+import GetInTouchScreen from './src/screen/GetInTouchScreen';
+import WelcomeScreen from './src/screen/WelcomeScreen';
 
+
+const Stack = createStackNavigator()
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="GetINTouch" component={GetInTouchScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
