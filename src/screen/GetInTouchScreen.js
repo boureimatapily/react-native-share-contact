4447d089f42858a515d9866029d710fe5edb50f7
boureimatapily <boureimatapily1@gmail.com>
2020-05-12 12:05:43 +0000

    MOD add class Components

---
+++ src/screen/GetInTouchScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+export default class GetInTouchScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    
+})
